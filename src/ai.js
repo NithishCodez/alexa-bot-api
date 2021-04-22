ee1b5d7b8b1701449ff9ea0aa4eaaac540915ecd
Major-Thrust <nithishdev509@gmail.com>
2021-04-22 13:10:23 +0530

    Fixed url error

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 module.exports = async (e, t) => {
   return fetch(
-    `https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${e}?lang=${t}`
+    encodeURI(`https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${e}?lang=${t}`)
   )
     .then((e) => e.json())
     .then((e) => e.reply);
--- src/tests.js
+++ src/tests.js
@@ -1,4 +1,4 @@
 const alex = require("./alexa.js");
 const ai = new alex();
 
-ai.getReply("hey%s", "spanish").then(console.log);
+ai.getReply("hey%sssd/;[';0)s", "spanish").then(console.log);
