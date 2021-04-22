8b76bdbcd2061e7630d58c3cdfb82f8f357586fd
Major-Thrust <nithishdev509@gmail.com>
2021-04-22 11:37:37 +0530

    😒 Decode uri component

--- src/ai.js
+++ src/ai.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 module.exports = async (e, t) => {
   return fetch(
-    `https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${e}?lang=${t}`
+    `https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${decodeURIComponent(e)}?lang=${decodeURIComponent(t)}`
   )
     .then((e) => e.json())
     .then((e) => e.reply);
