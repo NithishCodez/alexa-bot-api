9d65fa9748898cdad39dd44d9813c3b89eeb26c0
ANF-Studios <theanfstudios@gmail.com>
2021-04-26 00:36:36 -0400

    Fix undefined return value

--- src/ai.js
+++ src/ai.js
@@ -3,6 +3,5 @@ module.exports = async (e, t) => {
   return fetch(
     encodeURI(`https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${e}?lang=${t}`)
   )
-    .then((e) => e.json())
-    .then((e) => e.reply);
+    .then((e) => e.json().then(e => e.name))
 };
