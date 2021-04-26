7922508a663294167ec5b67387c33c750b6dd56f
ANF-Studios <theanfstudios@gmail.com>
2021-04-26 14:20:52 +0500

    Added CI and improved tests

--- src/tests.js
+++ src/tests.js
@@ -1,4 +1,10 @@
 const alex = require("./alexa.js");
 const ai = new alex();
 
-ai.getReply("hey%sssd/;[';0)s", "spanish").then(console.log);
+ai.getReply("hey%sssd/;[';0)s", "spanish").then((response) => {
+    if (response == null || response == undefined) // Add more checks here if needed.
+        exit(1); // If so, fail.
+    else
+        console.log(`Response: "${response}"`);
+    exit(0); // Pass if still here.
+});
