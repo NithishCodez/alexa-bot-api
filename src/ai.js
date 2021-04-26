a00483db7093aba2e8751de383537a20641ca7b4
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-04-26 02:02:06 -0400

    Changed property name

--- src/ai.js
+++ src/ai.js
@@ -3,5 +3,5 @@ module.exports = async (e, t) => {
   return fetch(
     encodeURI(`https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${e}?lang=${t}`)
   )
-    .then((e) => e.json().then(e => e.name))
+    .then((e) => e.json().then(e => e.reply))
 };
